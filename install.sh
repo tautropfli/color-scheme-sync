c077de7fc1d401fe98ff30a86971333b16081d39
Tau Gärtli <git@tau.garden>
2025-06-22 01:17:58 +0200

    Use %h placeholder in unit file

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+config_home="${XDG_CONFIG_HOME:-$HOME/.config}"
 mkdir -p ~/.local/bin/
 cp target/release/color-scheme-sync ~/.local/bin/color-scheme-sync
-mkdir -p ~/.config/systemd/user/
-envsubst < color-scheme-sync.service.in > ~/.config/systemd/user/color-scheme-sync.service
+mkdir -p "$config_home/systemd/user/"
+cp color-scheme-sync.service "$config_home/systemd/user/color-scheme-sync.service"
 
 systemctl enable --now --user color-scheme-sync
